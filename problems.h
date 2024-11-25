de12f733c48e4f973ed2040b9dadee4714950502
Jafarbek Yusupov <j.yusupov@newuu.uz>
2024-11-25 20:16:05 +0500

    Update problems.h

--- problems.h
+++ problems.h
@@ -1,10 +1,16 @@
 #include <vector>
 using namespace std;
 
-void delete11(vector<int> &v){    v.erase(remove_if(v.begin(), v.end(), [](int x) { return x % 11 == 0; }), v.end());}
+void delete11(vector<int> &v) {    
+  v.erase(remove_if(v.begin(), v.end(), [](int x) { return x % 11 == 0; }), v.end());
+}
 
-void insertMany ( vector < int > &v , int a , int b ) {
-  
+void insertMany(vector<int> &v, int x, int y) {
+    int md;
+    for (int i = 0; i < y; ++i) {
+        md = (v.size() % 2 == 0) ? (v.size() / 2) : (v.size() / 2) + 1;
+        v.insert(v.begin() + md, x);
+    }
 }
 
 int* allocateAndSet(int a, int b){
